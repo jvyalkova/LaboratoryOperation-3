ab43a7abdb7fbd3087252e24d656f28f5ad1104a
jvyalkova <146427213+jvyalkova@users.noreply.github.com>
2023-11-16 21:58:17 +0300

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -1,111 +1,56 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
-int findMaxPath(vector<vector<int>>& triangle)
-{
-    int n = triangle.size();
-
-    // Creating a copy of the triangle to store intermediate results
-    vector<vector<int>> dp(triangle);
-
-    // We pass through the triangle from the bottom up, starting from the second row
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            // Select the maximum value from three adjacent elements in the next row
-            dp[i][j] += max(dp[i + 1][j], max(dp[i + 1][j + 1], dp[i + 1][j + 2]));
-        }
-    }
-
-    // Return the maximum load value at the vertex of the triangle
-    return dp[0][0];
-}
-
-vector<int> findOptimalPath(vector<vector<int>>& triangle)
-{
-    int n = triangle.size();
-
-    // Creating a copy of the triangle to store intermediate results
-    vector<vector<int>> dp(triangle);
-
-    // We pass through the triangle from the bottom up, starting from the second row
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = 0; j <= i; j++)
-        {
-            // Select the maximum value from three adjacent elements in the next row
-            dp[i][j] += max(dp[i + 1][j], max(dp[i + 1][j + 1], dp[i + 1][j + 2]));
-        }
-    }
-
-    // Restoring the optimal path
-    vector<int> path;
-    int row = 0, col = 0;
-    path.push_back(triangle[row][col]);
-
-    while (row < n - 1)
-    {
-        if (dp[row + 1][col] > dp[row + 1][col + 1])
-        {
-            row++;
-        }
-        else if (dp[row + 1][col + 1] > dp[row + 1][col + 2])
-        {
-            row++;
-            col++;
-        }
-        else
-        {
-            row++;
-            col += 2;
-        }
-        path.push_back(triangle[row][col]);
-    }
-
-    return path;
-}
-
-int main()
+int main() 
 {
     setlocale(LC_ALL, "RUS");
-    // Opening the file for reading
-    ifstream inputFile("triangle.txt");
-    if (!inputFile)
-    {
-        cout << "Файл не найден." << endl;
-        return 1;
-    }
+    int size;
+    cout << "Введите размер треугольника: ";
+    cin >> size;
 
-    // Reading a triangle from a file
-    vector<vector<int>> triangle;
-    int num;
-    while (inputFile >> num)
+    vector<vector<int>> triangle(size, vector<int>(size));
+
+    for (int i = 0; i < size; i++) 
     {
-        vector<int> row;
-        for (int i = 0; i < num; i++)
+        for (int j = 0; j <= i; j++)
         {
-            int value;
-            inputFile >> value;
-            row.push_back(value);
+            triangle[i][j] = rand() % 100; 
         }
-        triangle.push_back(row);
     }
 
-    inputFile.close();
-
-    int maxSum = findMaxPath(triangle);
-    vector<int> optimalPath = findOptimalPath(triangle);
-
-    cout << "Максимальное суммарное значение нагрузок: " << maxSum << endl;
-    cout << "Оптимальный путь: " << " ";
-    for (int i = 0; i < optimalPath.size(); i++)
+    cout << "Треугольник:\n";
+    for (int i = 0; i < size; i++) 
     {
-        cout << optimalPath[i] << " ";
+        for (int j = 0; j <= i; j++) 
+        {
+            cout << triangle[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    vector<int> path;
+    int maxSum = triangle[0][0];
+    int maxIndex = 0;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (triangle[i][maxIndex] < triangle[i][maxIndex + 1]) 
+        {
+            maxIndex++;
+        }
+
+        maxSum += triangle[i][maxIndex];
+        path.push_back(maxIndex);
+    }
+
+    cout << "Максимальная сумма: " << maxSum << endl;
+    cout << "Путь: ";
+    for (int i = 0; i < path.size(); i++) 
+    {
+        cout << "(" << i << ", " << path[i] << ") ";
     }
     cout << endl;
 
